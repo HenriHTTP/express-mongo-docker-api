ea16563ac1c2f5dd67a514423ab9b78c75e90f79
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-09-10 23:23:33 -0300

    [feat] : add user controller and models

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,13 @@
 // user controler
+const users = require('../models/user.schema');
+
+module.exports = class userController {
+  static async CreateUser(req, res) {
+    try {
+      console.log(req.body);
+      res.status(200).json(req.body);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+};
--- models/user.schema.js
+++ models/user.schema.js
@@ -1,3 +1,24 @@
 // user sschemas
 
 const mongoose = require('../databases/conn');
+const { Schema } = mongoose;
+
+const users = mongoose.model(
+  'users',
+  new Schema({
+    username: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  }),
+);
+
+module.exports = users;
