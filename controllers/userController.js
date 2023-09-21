38f5144a6a19089be4e2ac5f3e8870cb5baa9690
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-09-21 18:50:11 -0300

    feat:  create controller  registeruser method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,22 @@
 // user controler
 const users = require('../models/user.schema');
+const bcrypt = require('bcryptjs');
 
 module.exports = class userController {
-  static async CreateUser(req, res) {
+  static async registerUser(req, res) {
+    const { username, email, password } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const cryptpassword = bcrypt.hashSync(password, salt);
+
+    const user = new users({
+      username,
+      email,
+      password: cryptpassword,
+    });
+
     try {
-      console.log(req.body);
-      res.status(200).json(req.body);
+      await user.save();
+      res.status(200).json(user);
     } catch (err) {
       res.status(500).json(err);
     }
