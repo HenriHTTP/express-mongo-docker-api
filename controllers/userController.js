8003e7cf081516e9ce2bab0f37443fbd0ccc60bd
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-09-23 22:19:41 -0300

    feat : add name lastname  and  token create

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,30 @@
-// user controler
 const users = require('../models/user.schema');
 const bcrypt = require('bcryptjs');
+const createToken = require('../helpers/createToken');
 
-module.exports = class userController {
+class userController {
+  // method login controller
   static async registerUser(req, res) {
-    const { username, email, password } = req.body;
+    const { name, lastname, username, email, password } = req.body;
     const salt = bcrypt.genSaltSync(10);
     const cryptpassword = bcrypt.hashSync(password, salt);
 
     const user = new users({
+      name,
+      lastname,
       username,
       email,
       password: cryptpassword,
     });
 
     try {
+      //save objeto in database
       await user.save();
-      res.status(200).json(user);
+      await createToken(user, req, res);
     } catch (err) {
       res.status(500).json(err);
     }
   }
-};
+}
+
+module.exports = userController;
