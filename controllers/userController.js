1ac752c1a7591811ed49c7d7123f3e7c76b7f554
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-09-23 23:56:24 -0300

    feat :  login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const createToken = require('../helpers/createToken');
 
 class userController {
-  // method login controller
+  // method register controller
   static async registerUser(req, res) {
     const { name, lastname, username, email, password } = req.body;
     const salt = bcrypt.genSaltSync(10);
@@ -20,6 +20,19 @@ class userController {
     try {
       //save objeto in database
       await user.save();
+      await createToken(user, req, res);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //method login controller
+  static async loginUser(req, res) {
+    try {
+      const { email } = req.body;
+      const user = await users.findOne({ email: email });
+
+      //create new token
       await createToken(user, req, res);
     } catch (err) {
       res.status(500).json(err);
