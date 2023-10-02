0b65f7ebdf9a7ec86b3092f45187835055ec9d7c
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-01 23:54:46 -0300

    feat : add invalid signature token

--- validations/token.validation.js
+++ validations/token.validation.js
@@ -1,5 +1,6 @@
 const JWT = require('jsonwebtoken');
 const get_token = require('../helpers/get_token');
+const errorMenssages = require('../error/errorMenssages');
 const path = require('path');
 const dotenv = require('dotenv');
 
@@ -9,24 +10,33 @@ async function tokenValidation(req, res, next) {
   try {
     //get authorization to request
     if (!req.headers.authorization) {
-      res.status(400).json({ error: 'authorization is not found' });
+      res.status(400).json(errorMenssages.authorizationNotFound());
     }
 
     // get token by request
     const token = await get_token(req);
     if (!token) {
-      res.status(400).json({ error: 'token is not found' });
+      res.status(400).json(errorMenssages.tokenNotFound());
     }
 
     // verify token
     const tokenIsValid = JWT.verify(token, process.env.SECRET);
     if (!tokenIsValid) {
-      res.status(400).json({ error: 'token is invalid' });
+      res.status(400).json(errorMenssages.tokenNotFound());
     }
 
+    const decodeToken = JWT.decode(token, process.env.SECRET);
+
+    if (!decodeToken) {
+      res.status(400).json(errorMenssages.tokenNotFound());
+    }
     next();
   } catch (err) {
-    res.json({ error: `${err}` });
+    if (err instanceof JWT.JsonWebTokenError) {
+      res.status(500).json(errorMenssages.invalidSignature());
+    } else {
+      res.json({ error: `${err}` });
+    }
   }
 }
 
