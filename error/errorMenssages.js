f2a424aa757d4f5258d99cfa4050a85f7b2cb667
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-01 23:53:24 -0300

    feat : add error validation token

--- error/errorMenssages.js
+++ error/errorMenssages.js
@@ -81,6 +81,27 @@ class errorMessages {
       )
       .getMessage();
   }
+  tokenNotFound() {
+    return this.setStatus(404)
+      .setError('the Token is not found')
+      .setDetails(
+        'The provided token is not found. Please provide a valid token.',
+      );
+  }
+  authorizationNotFound() {
+    return this.setStatus(404)
+      .setError('authorization is not found')
+      .setDetails(
+        'The authorization header is missing. Please include a valid authorization header.',
+      );
+  }
+  invalidSignature() {
+    return this.setStatus(500)
+      .setError('signature is not found')
+      .setDetails(
+        'The signature token is missing. Please include a valid signature token',
+      );
+  }
 }
 
 module.exports = new errorMessages();
