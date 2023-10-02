2cb4ec697cf70ff094c22e1ae57713d4302f1d2c
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-01 22:48:39 -0300

    feate:  create route check user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,8 +5,10 @@ const userController = require('../controllers/userController');
 //validations
 const registerValidation = require('../validations/Register.validation');
 const loginValidation = require('../validations/login.validation');
+const tokenValidation = require('../validations/tokenValidation');
 
 router.post('/register', registerValidation, userController.registerUser);
 router.post('/login', loginValidation, userController.loginUser);
+router.get('/checkuser', tokenValidation, userController.checkuser);
 
 module.exports = router;
