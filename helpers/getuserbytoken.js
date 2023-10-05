434e4d975459c948d48dbbc68fc154b1bd86e517
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-04 22:28:50 -0300

    feat:  create  get user by token

---
+++ helpers/getuserbytoken.js
@@ -0,0 +1,12 @@
+const users =  require('../models/user.schema')
+const decodeToken = require('./decodeToken')
+
+
+async function getuserbyToken(req){ 
+    const token = await decodeToken(req)
+    const user =  await users.findOne({_id:token.id})
+    if((!user)) return 
+    return user
+}
+
+module.exports =  getuserbyToken; 
