9605fe015add02bb4848a9160ff14cb5afc4546a
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-07 20:26:57 -0300

    reactor : add identation

--- helpers/getuserbytoken.js
+++ helpers/getuserbytoken.js
@@ -1,12 +1,11 @@
-const users =  require('../models/user.schema')
-const decodeToken = require('./decodeToken')
+const users = require('../models/user.schema');
+const decodeToken = require('./decodeToken');
 
-
-async function getuserbyToken(req){ 
-    const token = await decodeToken(req)
-    const user =  await users.findOne({_id:token.id})
-    if((!user)) return 
-    return user
+async function getuserbyToken(req) {
+  const token = await decodeToken(req);
+  const user = await users.findOne({ _id: token.id });
+  if (!user) return;
+  return user;
 }
 
-module.exports =  getuserbyToken; 
+module.exports = getuserbyToken;
