53f228ac1d0656073455bca8531c79a8005b8c14
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-07 20:28:44 -0300

    feat:  remove variable not used and ident

--- validations/login.validation.js
+++ validations/login.validation.js
@@ -1,45 +1,49 @@
 const users = require('../models/user.schema');
 const validator = require('validator');
-const errorMenssages = require('../error/errorMenssages');
+const errorLogin = require('../error/errorLogin');
 const bcrypt = require('bcryptjs');
 
 async function LoginValidations(req, res, next) {
   try {
-    //require params
-    const { name, lastname, email, username, password, confirmpassword } =
-      req.body;
+    // require params
+    const { email, username, password } = req.body;
 
-    const requirefields = ['username', 'password', 'email'];
+    const requiredFields = ['username', 'password', 'email'];
 
-    // validationn  if required values  is  null
-    for (const fields of requirefields) {
-      if (!req.body[fields]) {
-        return res.status(400).json(errorMenssages.requiredField(fields));
+    // validation if required values are null
+    for (const field of requiredFields) {
+      if (!req.body[field]) {
+        return res.status(400).json(errorLogin.requiredField(field));
       }
     }
 
-    // validationn  if email is already in use
+    // validation email pattern
+    if (!validator.isEmail(email)) {
+      return res.status(400).json(errorLogin.invalidEmailFormat());
+    }
+
+    // validation if email is already in use
     const emailExist = await users.findOne({ email: email });
     if (!emailExist) {
-      return res.status(400).json(errorMenssages.userNotFound());
+      return res.status(400).json(errorLogin.userNotFound());
     }
 
-    // validationn  if username is already in use
+    // validation if username is already in use
     const usernameExist = await users.findOne({ username: username });
     if (!usernameExist) {
-      return res.status(400).json(errorMenssages.userNotFound());
+      return res.status(400).json(errorLogin.usernameInUse());
     }
 
-    // validationn  if passaword is invalid
+    // validation if password is invalid
     const user = await users.findOne({ email: email });
     const passwordMatch = bcrypt.compareSync(password, user.password);
 
     if (!passwordMatch) {
-      return res.status(401).json(errorMenssages.userNotFound());
+      return res.status(401).json(errorLogin.userNotFound());
     }
     next();
   } catch (err) {
-    return err;
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 }
 
