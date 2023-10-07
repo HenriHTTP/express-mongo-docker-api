12783ff9f28df21e1d3e4ed3bbeb59b342ca1696
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-07 20:25:18 -0300

    reafactor:  add new identation

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,8 @@ const users = require('../models/user.schema');
 const bcrypt = require('bcryptjs');
 const createToken = require('../helpers/createToken');
 const decodeToken = require('../helpers/decodeToken');
-const get_token  = require('../helpers/get_token');
-const getuserbyToken = require('../helpers/getuserbytoken')
+const get_token = require('../helpers/get_token');
+const getuserbyToken = require('../helpers/getuserbytoken');
 
 class userController {
   // method register controller
@@ -44,57 +44,53 @@ class userController {
 
   //method getuserbytolken
   static async checkuser(req, res) {
-    try{ 
+    try {
       const token = await decodeToken(req);
       const user = await users.findOne({ _id: token.id }, { password: 0 });
 
       res.status(200).json(user);
-    }catch(err){
-      res.status(500).json({error: `${err}`})
+    } catch (err) {
+      res.status(500).json({ error: `${err}` });
     }
   }
 
   // method get user by id
-  static async GetUserById(req,res){ 
-    try{ 
-      const  id = req.params.id 
-      const user =  await users.findOne({_id:id})
+  static async GetUserById(req, res) {
+    try {
+      const id = req.params.id;
+      const user = await users.findOne({ _id: id });
 
-      res.status(200).json(user)
-
-    }catch(err){ 
-      res.status(500).json({error:`${err}`})
-    }  
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json({ error: `${err}` });
+    }
   }
 
   //method edit informations user
-  static async editUser(req,res){
-    try{ 
-      const user = await getuserbyToken(req)
-      const {name, lastname, email, username, password } = req.body
+  static async editUser(req, res) {
+    try {
+      const user = await getuserbyToken(req);
+      const { name, lastname, email, username, password } = req.body;
       const salt = bcrypt.genSaltSync(10);
       const cryptpassword = bcrypt.hashSync(password, salt);
-      const newDataUser = { 
-          name,
-          lastname,
-          email, 
-          username, 
-          password: cryptpassword,
-         }
+      const newDataUser = {
+        name,
+        lastname,
+        email,
+        username,
+        password: cryptpassword,
+      };
 
       const updateUser = await users.findOneAndUpdate(
-        {_id:user._id},
-        {$set:newDataUser},
-        {new:true},
-        )   
-      res.status(200).json(updateUser)   
-  
-    }catch(err){ 
-      res.status(500).json({error:`${err}`})
+        { _id: user._id },
+        { $set: newDataUser },
+        { new: true },
+      );
+      res.status(200).json(updateUser);
+    } catch (err) {
+      res.status(500).json({ error: `${err}` });
     }
- 
   }
-  
 }
 
 module.exports = userController;
