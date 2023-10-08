469083002883c8522d26d45d783b66178f470840
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-07 23:37:24 -0300

    reafactor: add tokeIsValid

--- validations/getUser.validation.js
+++ validations/getUser.validation.js
@@ -5,11 +5,13 @@ const errorToken = require('../error/errorToken');
 async function getUserValidation(req, res, next) {
   try {
     // validation if email has refence in token
-    const tokenUser = await getuserbyToken(req);
-    const idToken = await users.findOne({
-      _id: tokenUser._id,
+    const token = await getuserbyToken(req);
+    const user = await users.findOne({
+      _id: token._id,
     });
-    if (req.params.id != idToken._id) {
+
+    const tokenIsValid = req.params.id == user._id;
+    if (!tokenIsValid) {
       return res.status(400).json(errorToken.invalidSignature());
     }
     next();
