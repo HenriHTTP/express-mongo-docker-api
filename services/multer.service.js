ef0e99d8e688ee32ba40edb785cf40662ec1b192
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-12 18:49:57 -0300

    feat: remove  storage multer

--- services/multer.service.js
+++ services/multer.service.js
@@ -1,21 +1,11 @@
-// config multer for uploads
 const multer = require('multer');
 const path = require('path');
 const uploadStore = path.resolve(__dirname, '../upload');
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, uploadStore);
-  },
-  filename: (req, file, callback) => {
-    const timeUploadArquive = new Date().getTime();
-    const arquiveName = file.originalname;
-    callback(null, `${timeUploadArquive}_${arquiveName}`);
-  },
-});
+const memostorage = multer.memoryStorage();
 
 const imageUpload = multer({
-  storage: storage,
+  storage: memostorage,
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) {
       cb(null, true);
