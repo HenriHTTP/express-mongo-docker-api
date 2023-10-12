b9c648bbdf4f201c77b17040aaf8b1fda5308976
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-12 18:47:38 -0300

    feat: create error upload

--- error/errorUpload.js
+++ error/errorUpload.js
@@ -1,1 +1,23 @@
 // uploads error
+const errorMessages = require('./errorMenssages');
+
+class errorUpload extends errorMessages {
+  invalidImage() {
+    return this.setStatus('error')
+      .setError('Image invalid')
+      .setDetails(
+        'The provided email address is not in a valid format.to send other image',
+      )
+      .getMessage();
+  }
+  arquiveNotFound() {
+    return this.setStatus(404)
+      .setError('arquive not found or not to sent')
+      .setDetails(
+        'The provided arquive is not found. Please provide a valid arquive.',
+      )
+      .getMessage();
+  }
+}
+
+module.exports = new errorUpload();
