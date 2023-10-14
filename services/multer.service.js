539eb79711d76a6b7b1a168c9cce7297c47574bd
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-13 22:14:49 -0300

    refactor:  simplify multer for storage memory

--- services/multer.service.js
+++ services/multer.service.js
@@ -1,18 +1,7 @@
 const multer = require('multer');
-const path = require('path');
-const uploadStore = path.resolve(__dirname, '../upload');
 
-const memostorage = multer.memoryStorage();
-
+const storage = multer.memoryStorage();
 const imageUpload = multer({
-  storage: memostorage,
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith('image/')) {
-      cb(null, true);
-    } else {
-      cb(new Error('Apenas arquivos de imagem são permitidos.'));
-    }
-  },
-  limits: { fileSize: 3 * 1024 * 1024 }, // 3 MB
+  storage: storage,
 });
 module.exports = { imageUpload };
