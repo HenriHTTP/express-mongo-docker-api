ed86c0dce440cb14c5943db05543411499a693ea
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-17 21:49:52 -0300

    remove: imports not used

--- helpers/token/get_token.js
+++ helpers/token/get_token.js
@@ -1,14 +1,7 @@
-const JWT = require('jsonwebtoken');
-const path = require('path');
-const dotenv = require('dotenv');
-
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
-
 async function get_token(req) {
   //token req
   const authorization = req.headers.authorization;
   const token = authorization.split(' ')[1];
-
   return token;
 }
 
