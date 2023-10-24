9277e88e715423997e845df9a507a8f13a2865bd
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-23 22:28:22 -0300

    feat: add order route

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 // global routes
 const express = require('express');
 const router = express.Router();
+
+//routes
 const userRoutes = require('./user.routes');
+const orderRoutes = require('./order.routes');
 
 router.use('/api/user', userRoutes);
+router.use('/api/order', orderRoutes);
 
 module.exports = router;
