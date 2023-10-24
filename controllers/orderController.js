a73d76bd8873780a84fd76c65d278d078cea5e8a
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-10-23 22:26:29 -0300

    feat: create method create order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,1 +1,22 @@
 //order controller
+const orders = require('../models/order.schema');
+
+class orderController {
+  static async createOrder(req, res) {
+    const { usernameClient, usernameProvider, type } = req.body;
+    const newOrder = new orders({
+      usernameClient,
+      usernameProvider,
+      type,
+    });
+
+    try {
+      await newOrder.save();
+      const order = await orders.findOne({ usernameClient: usernameClient });
+      res.status(200).json(order);
+    } catch (err) {
+      res.json(err);
+    }
+  }
+}
+module.exports = orderController;
